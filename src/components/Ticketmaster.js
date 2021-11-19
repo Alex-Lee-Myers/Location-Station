1f1e227269005c1e453bef7622cb408ff63ecd3d
Luna Silver <drhowell714@gmail.com>
2021-11-18 20:52:20 -0500

    1

---
+++ src/components/Ticketmaster.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import { Container, Row, Col, Button } from 'reactstrap'
+
+const Ticket = () => {
+    const [info, setInfo] = useState("");
+    const [info2, setInfo2] = useState("");
+    const [info3, setInfo3] = useState("");
+    const [info4, setInfo4] = useState("");
+    const [info5, setInfo5] = useState("");
+}
