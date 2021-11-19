764747b07175f1cb8f377a4db4e91e23be62da3f
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2021-11-18 20:45:10 -0500

    able to hit button now, and grab their longitude and latitude.

---
+++ src/components/OpenWeather.js
@@ -0,0 +1,13 @@
+import {Row} from 'reactstrap';
+
+const OpenWeather = () => {
+    return(
+        <footer>
+            <Row>
+                <p>Open Weather</p>
+            </Row>
+        </footer>
+    );
+};
+
+export default OpenWeather;
