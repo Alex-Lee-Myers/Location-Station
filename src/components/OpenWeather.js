51b0ca0e8f2f422066b9a79a14a17b7ac63644b8
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2021-11-21 17:40:28 -0500

    fetch working. darn you HTTPS:...everything else was right. Button works too for C/F switching. icon too. yeah.

--- src/components/OpenWeather.js
+++ src/components/OpenWeather.js
@@ -1,45 +1,52 @@
-import {useState} from 'react';
-import {Row} from 'reactstrap';
+import { useState } from 'react';
+import { Row } from 'reactstrap';
+import { owAPIKey } from '../environment';
 
-const OpenWeather = (props) => {
-    const apikey = "64bfb013b2e8badaa4709707c0aa580b";
-    console.log("props.latitude:", props.latitude)
-    console.log("props.longitude:", props.longitude)
-    const owAPI = `api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&appid=${apikey}`
-    console.log("owAPI:", owAPI)
+const OpenWeather = (props) => {
+    const [currentWeather, setCurrentWeather] = useState({ temp: 0,icon: '',description: ''});
+    const getCurrentWeather = async () => {
+        const apiKey = owAPIKey;
+        const owAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&appid=${apiKey}`
+        await fetch(owAPI)
+            .then((res) => {
+                const response = res.json();
+                return response;
+            })
+            .then((data) => {
+                setCurrentWeather({
+                    temp: data.main.temp - 273.15, //default to Celsius
+                    icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+                    description: data.weather[0].description
+                })
+            })
+            .catch(err => console.log(err));
+    }
 
     const [isFahrenheit, setIsFahrenheit] = useState(false);
     const toggleFahrenheit = () => {
-        setIsFahrenheit(!isFahrenheit);
+        setIsFahrenheit(!isFahrenheit)
+            setCurrentWeather(prevWeather => ({
+                ...prevWeather,
+                temp: !isFahrenheit ? (currentWeather.temp * (9 / 5)) + 32 : (currentWeather.temp - 32) * (5 / 9)
+            }))
     }
-
-    const [currentWeather, setCurrentWeather] = useState({temp: ''});
-    const getCurrentWeather = () => {
-        fetch(owAPI)
-        .then(response => response.json())
-        .then(data => {
-            setCurrentWeather({
-                temp: data.main.temp,
-                icon: data.weather[0].icon,
-                description: data.weather[0].description
-            })
-        })
-    }
-    getCurrentWeather();
 
     // show weather and toggle between fahrenheit and celsius in return
     return (
         <div>
             <Row>
                 <h3>Current Weather:</h3>
-                <h1>{Math.round(currentWeather.temp)}°</h1>
-                <button onClick={toggleFahrenheit}>{isFahrenheit ? "Celsius" : "Fahrenheit"}</button>
+                <button onClick={getCurrentWeather}>Get Weather</button>
+                <h1>Temperature: {Math.round(currentWeather.temp)}°{isFahrenheit ? "F":"C"}</h1>
+                <button onClick={toggleFahrenheit}>Convert to {isFahrenheit ? "Celsius" : "Fahrenheit"}</button>
             </Row>
             <Row>
-                <p>{currentWeather.description}</p>
+                <p>Description: {currentWeather.description}</p>
+                <p>Icon: <img src={currentWeather.icon}></img></p>
+                
             </Row>
         </div>
     )
 }
 
-export default OpenWeather;
+export default OpenWeather;
