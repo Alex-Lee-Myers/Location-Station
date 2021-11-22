30a6054430660ff91f928faa3952b10ada4d7c03
Alex-Lee-Myers <alexander.l.myers@gmail.com>
2021-11-21 22:44:22 -0500

    adding env.js for owAPIkey. couldn't figure out how to hide the key while sharing with teammates.

---
+++ src/environment.js
@@ -0,0 +1,5 @@
+const owAPIKey = '2de6705d788ff0ce77cd693b4a3cf03d';
+const nasaAPIKey = 'ikkhQdcsxgyzwJGeXOKNvAhjidPDUtK1jKnzdxux';
+const nasaAPIurl = 'https://api.nasa.gov/planetary/apod?api_key=' + nasaAPIKey;
+
+export {owAPIKey, nasaAPIKey, nasaAPIurl};
